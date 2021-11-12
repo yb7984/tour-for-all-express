90705bef898caaddbc3b7429c7591248afbd7d6a
Sunbao Wu <bobowu@outlook.com>
2021-11-12 06:15:06 -0500

    fix not logging error during test

--- routes/ws.js
+++ routes/ws.js
@@ -25,7 +25,7 @@ function broadcastByEndpoint(endpoint, data) {
             client.send(data)
         });
     } catch (error) {
-        console.error(error);
+        if (process.env.NODE_ENV !== "test") console.error(error);
     }
 }
 
